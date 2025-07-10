252e8223165d76852f60cddaeca590b4ff3962d6
AMoir117 <andymoir5@gmail.com>
2025-07-09 17:13:51 -0700

    heroku deployment edits

--- main.py
+++ main.py
@@ -7,10 +7,9 @@ this project will be;
 
 import http.server
 import socketserver
-import webbrowser
 import os
 
-PORT = 8000
+PORT = int(os.environ.get("PORT", 8000))  # Use Heroku's port if available
 DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 
 class Handler(http.server.SimpleHTTPRequestHandler):
@@ -18,8 +17,6 @@ class Handler(http.server.SimpleHTTPRequestHandler):
         super().__init__(*args, directory=DIRECTORY, **kwargs)
 
 if __name__ == "__main__":
-    print(f"Serving at http://localhost:{PORT}")
-    webbrowser.open(f"http://localhost:{PORT}")
+    print(f"Serving at http://0.0.0.0:{PORT}")
     with socketserver.TCPServer(("", PORT), Handler) as httpd:
         httpd.serve_forever()
-
